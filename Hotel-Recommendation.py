7c3da7c9ee9b4d9068092f191cf87477648cffbf
Ngoc Bich <ngocbich.892k1@gmail.com>
2025-09-16 22:02:04 +0700

    Update Hotel-Recommendation.py

--- Hotel-Recommendation.py
+++ Hotel-Recommendation.py
@@ -21,8 +21,8 @@ from reportlab.lib import colors
 @st.cache_data
 def load_data():  
     # Load bằng pandas
-    hotel_info = pd.read_csv("data_clean/hotel_info.csv"))
-    hotel_comments = pd.read_csv("data_clean/hotel_comments.csv"))
+    hotel_info = pd.read_csv("data_clean/hotel_info.csv")
+    hotel_comments = pd.read_csv("data_clean/hotel_comments.csv")
     hotel_comments["Review_Date"] = pd.to_datetime(hotel_comments["Review_Date"], errors="coerce")
     hotel_corpus_cosine = pd.read_csv("data_clean/hotel_corpus_cosine.csv"))
     return hotel_info, hotel_comments, hotel_corpus_cosine
